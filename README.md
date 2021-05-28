827f677f5cabd1c0daa850c9a8341299c5d6c741
Aryan Behnam <aryan.behnam@hansrobot.de>
2021-05-28 10:35:19 +0200

    feat: add AddMovie component, add POST request, setup DB

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Movies Lsit
 
 ## Installation
+
 Navigate to `movies-list` and then run the following commands:
 `npm install`
 `npm run start`
 
+### Format the code
 
-### Format the code 
 Run `npm run format`
 
 ### Running ESlint
+
 Run `npm run lint`
--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { useState, useEffect, useCallback } from 'react'
 
-import MoviesList from './components/MoviesList/index'
+import MoviesList from './components/Movies/MoviesList'
+import AddMovie from './components/Movies/AddMovie'
 import './App.css'
 import loading from './assets/img/loading.gif'
 
 const App = () => {
+    const URL = process.env.REACT_APP_URL
+
     const [movies, setMovies] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     const fetchMoviesHandler = useCallback(async () => {
         setIsLoading(true)
-        const response = await fetch('https://swapi.dev/api/films/')
-        // destructured of data.results
-        const { results } = await response.json()
-        setMovies(results)
+        setError(null)
+        try {
+            const response = await fetch(`${URL}/movies.json`)
+            if (!response.ok) {
+                throw new Error('Something went wrong!')
+            }
+
+            const data = await response.json()
+
+            const loadedMovies = []
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingCrawl: data[key].openingCrawl,
+                    director: data[key].director,
+                    releaseDate: data[key].releaseDate
+                })
+            }
+
+            setMovies(loadedMovies)
+        } catch (error) {
+            setError(error.message)
+        }
 
         setTimeout(() => {
             setIsLoading(false)
@@ -24,16 +49,42 @@ const App = () => {
         fetchMoviesHandler()
     }, [fetchMoviesHandler])
 
+    const addMovieHandler = async (movie) => {
+        const response = await fetch(`${URL}/movies.json`, {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const data = await response.json()
+        console.log(data)
+    }
+
+    let content = <p>No Movie found.</p>
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies} />
+    }
+
+    if (error) {
+        content = <p>{error}</p>
+    }
+
+    if (isLoading) {
+        content = <img src={loading} alt="loading" />
+    }
+
     return (
         <React.Fragment>
             <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
+            <section>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
-
-            <section>
-                {!isLoading && movies.length === 0 && <p>No Movie found.</p>}
-                {!isLoading ? <MoviesList movies={movies} /> : <img src={loading} alt="loading" />}
-            </section>
+            <section>{content}</section>
         </React.Fragment>
     )
 }
